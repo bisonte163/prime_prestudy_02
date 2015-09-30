6f8bf1101fa1a0690638f22585e67457f175b3d6
bisonte163 <jason.tenbrink@gmail.com>
2015-09-29 20:49:47 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+avengersMovie = {
+    name: "Avengers",
+    runtime: "143 mins",
+    releaseYear: 2012,
+    Director: "Joss Whedon" 
+    //quotes around "Director" were fine, but unecessary
+
+}; 
+console.log(avengersMovie);
